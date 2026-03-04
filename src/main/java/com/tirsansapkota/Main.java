933cba0d92c8dfa4a87f17b56376d3e8b7858c21
Tirsan Sapkota <tirsanspkt159@gmail.com>
2026-03-04 13:21:32 -0500

    Phrase-O-Matic

--- src/main/java/com/tirsansapkota/Main.java
+++ src/main/java/com/tirsansapkota/Main.java
@@ -1,32 +1,44 @@
-
 package com.tirsansapkota;
 
 public class Main {
-    public static void main(String[] args) {
-        int bottlesNum = 10;
-        String word = "bottles";
+    public static void main(String[] args)
+    {
+        // Make three sets of words to choose from
+        String[] wordListOne = {"agnostic", "opinionated",
+        "voice activated", "haptically driven", "extensible",
+                "reactive", "agent based", "functional", "AI enabled", "strongly typed",
+        "stupid", "narcissistic", "like Tirsan"};
 
-        while (bottlesNum > 0) {
+        String[] wordListTwo = {
+                "loosely coupled", "six sigma",
+        "asynchronous", "event driven", "pub-sub", "IoT", "cloud native",
+                "service oriented", "containerized", "serverless",
+        "microservices", "distributed ledger", "dockered", "serverfull", "cloud, which means Baadal"};
 
-            if (bottlesNum == 1) {
-                word = "bottle";    // Singular, as in ONE Bottle.
-            }
+        String[] wordListThree = {
+                "framework", "library",
+        "DSL", "REST API", "repository", "pipeline", "service mesh", "architecture",
+                "perspective", "design", "orientation", "structure", "infrastructure", "megastructure"
+        };
 
-            System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
-            System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
-            System.out.println("And if one green bottle should accidentally fall,");
-            bottlesNum = bottlesNum - 1;
+        // Find out how many words are in each list
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
 
-            if (bottlesNum > 1) {
-                System.out.println("There will be "  + bottlesNum + " green " + word + ", hanging on the wall");
-            }
-            else if (bottlesNum > 0) {
-                System.out.println("There will be "  + bottlesNum + " green bottle, hanging on the wall");
-            }
-            else {
-                System.out.println("There will be no green bottles, hanging on the wall");
-            }
-            System.out.println("--------------------------------------------------------------------------");
-        }
+        // generate three random numbers
+        java.util.Random randomGenerator = new java.util.Random();
+        int rand1 = randomGenerator.nextInt(oneLength);
+        int rand2 = randomGenerator.nextInt(twoLength);
+        int rand3 = randomGenerator.nextInt(threeLength);
+
+        // Now build a phrase
+        String phrase = wordListOne[rand1] +  " " +
+                wordListTwo[rand2] + " " +
+                wordListThree[rand3];
+
+        // print out the phrase
+        System.out.println(phrase);
+
     }
 }
